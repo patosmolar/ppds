3136a2ee6b419b4827fb46648cac3b1b0f30f0da
patosmolar <patosmolar@gmail.com>
2021-02-22 17:32:41 +0100

    feat: added mutex versions of flow
    Added 3 versions of core method, which implement mutex

--- cv1/main.py
+++ cv1/main.py
@@ -20,19 +20,42 @@ class Histogram(dict):
             self[item] = self.get(item, 0) + 1
 
             
-def fnc(shared):
+def fnc1(shared):
     while True:
         if shared.counter >= shared.end:
             break
+        shared.mutex.lock()
         shared.array[shared.counter] += 1
         shared.counter += 1
+        shared.mutex.unlock()
+
+
+def fnc2(shared):
+    while True:
+        shared.mutex.lock()
+        if shared.counter >= shared.end:
+            break
+        shared.array[shared.counter] += 1
+        shared.mutex.unlock()       
+        shared.counter += 1
+
+        
+def fnc3(shared):
+    while shared.counter < shared.end:
+        shared.mutex.lock()
+        if shared.counter >= shared.end:
+            shared.mutex.unlock()
+            break
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
 
 
 
 for _ in range(10):
     sh = Shared(1000000)
-    t1 = Thread(fnc, sh)
-    t2 = Thread(fnc, sh)
+    t1 = Thread(fnc3, sh)
+    t2 = Thread(fnc3, sh)
 
     t1.join()
     t2.join()
