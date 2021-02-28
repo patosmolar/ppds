b9928041327d4e5afa71112dba708bbd5fd48236
patosmolar <patosmolar@gmail.com>
2021-02-28 19:07:58 +0100

    feat:Added Fibonacci sequence(semaphore)
    Added Fibonacci sequence with use of semaphores

--- cv2/main.py
+++ cv2/main.py
@@ -1,6 +1,6 @@
 from random import randint
 from time import sleep
-from fei.ppds import Thread, Semaphore, Mutex,Event, print
+from fei.ppds import Thread, Semaphore, Mutex, Event, print
 
 
 class SimpleBarrier:
@@ -27,7 +27,7 @@ class SimpleBarrier:
         self.count += 1
         if self.count == self.n:
             self.count = 0
-            self.event.signal()   
+            self.event.signal()
         self.mutex.unlock()
         self.event.wait()
 
@@ -35,43 +35,67 @@ class SimpleBarrier:
 def uloha_1(barrier, thread_id):
     sleep(randint(1, 10) / 10)
     print("vlakno %d pred barierou" % thread_id)
-    #barrier.wait_with_semaphore
     barrier.wait_with_events()
     print("vlakno %d po bariere" % thread_id)
 
+
 def rendezvous(thread_name):
-    sleep(randint(1,10)/10)
+    sleep(randint(1, 10)/10)
     print('rendezvous: %s' % thread_name)
- 
- 
+
+
 def ko(thread_name):
     print('ko: %s' % thread_name)
-    sleep(randint(1,10)/10)
- 
- 
-def uloha_2(b1,b2,thread_name):
+    sleep(randint(1, 10)/10)
+
+
+def uloha_2(b1, b2, thread_name):
     """Kazde vlakno vykonava kod funkcie 'barrier_example'.
     Doplnte synchronizaciu tak, aby sa vsetky vlakna pockali
     nielen pred vykonanim funkcie 'ko', ale aj
     *vzdy* pred zacatim vykonavania funkcie 'rendezvous'.
     """
- 
+
     while True:
         rendezvous(thread_name)
         b1.wait_with_events()
         ko(thread_name)
         b1.wait_with_events()
- 
- 
 
 
-barr1 = SimpleBarrier(5)
-barr2 = SimpleBarrier(5)
+class SharedObject:
+    def __init__(self, n):
+        self.n = n
+        self.mutex = Mutex()
+        self.fib = [0, 1] + [-1]*n
+        self.semaphores = [0] * (n+2)
+        for i in range(n+2):
+            self.semaphores[i] = Semaphore(0)
+        self.semaphores[0].signal(1)
+        self.semaphores[1].signal(2)
 
+    def perform_fib_semaphore(self, i):
+        self.semaphores[i - 1].wait()
+        self.semaphores[i - 2].wait()
+        self.fib[i] = self.fib[i-1] + self.fib[i-2]
+        self.semaphores[i].signal(2)
+
+
+def uloha_3(shared, id):
+    #print("in %s" % (id))
+    shared.perform_fib_semaphore(id)
+    #print("out %s" % (id))
+
+
+n_threads = 10
 threads = list()
-for i in range(5):
-    t = Thread(uloha_2, barr1,barr2, f'Thread {i}')
-    threads.append(t)
- 
+
+shared = SharedObject(n_threads)
+
+for id in reversed(range(n_threads)):
+    threads.append(Thread(uloha_3, shared, id+2))
+
 for t in threads:
     t.join()
+
+print(shared.fib)
