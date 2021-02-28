0765c116f4e7d39f1f5e43d864984961ce61eaa0
patosmolar <patosmolar@gmail.com>
2021-02-28 16:37:30 +0100

    feat: Added SimpleBarrier withevents

--- cv2/main.py
+++ cv2/main.py
@@ -1,6 +1,7 @@
 from random import randint
 from time import sleep
-from fei.ppds import Thread, Semaphore, Mutex, print
+from fei.ppds import Thread, Semaphore, Mutex,Event, print
+
 
 class SimpleBarrier:
     def __init__(self, n):
@@ -8,8 +9,9 @@ class SimpleBarrier:
         self.count = 0
         self.mutex = Mutex()
         self.semaphore = Semaphore(0)
+        self.event = Event()
 
-    def wait(self):
+    def wait_with_semaphore(self):
         self.mutex.lock()
         self.count += 1
         if self.count == self.n:
@@ -18,22 +20,31 @@ class SimpleBarrier:
         self.mutex.unlock()
         self.semaphore.wait()
 
+    def wait_with_events(self):
+        self.mutex.lock()
+        if(self.count == 0):
+            self.event.clear()
+        self.count += 1
+        if self.count == self.n:
+            self.count = 0
+            self.event.signal()   
+        self.mutex.unlock()
+        self.event.wait()
+
+
 def uloha_1(barrier, thread_id):
     sleep(randint(1, 10) / 10)
     print("vlakno %d pred barierou" % thread_id)
-    barrier.wait()
+    #barrier.wait_with_semaphore
+    barrier.wait_with_events()
     print("vlakno %d po bariere" % thread_id)
 
 barrier = SimpleBarrier(5)
 
-t1 = Thread(uloha_1, barrier, 1)
-t2 = Thread(uloha_1, barrier, 2)
-t3 = Thread(uloha_1, barrier, 3)
-t4 = Thread(uloha_1, barrier, 4)
-t5 = Thread(uloha_1, barrier, 5)
-
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
+threads = list()
+for i in range(5):
+    t = Thread(uloha_1, barrier, i)
+    threads.append(t)
+ 
+for t in threads:
+    t.join()
