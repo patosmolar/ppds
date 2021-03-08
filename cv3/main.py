4dc92e2dcb6593401dd6bdf121099da3b71df5ea
patosmolar <patosmolar@gmail.com>
2021-03-08 22:32:00 +0100

    feat: Add read/write function
    Add basic read/write function, without protection against starvation

--- cv3/main.py
+++ cv3/main.py
@@ -1,4 +1,6 @@
-from fei.ppds import Mutex, Semaphore
+from fei.ppds import Mutex, Semaphore, Thread, print
+from time import sleep
+from random import randint
 
 
 class LightSwitch(object):
@@ -19,3 +21,44 @@ class LightSwitch(object):
         if self.counter == 0:
             semaphore.signal()
         self.mutex.unlock()
+
+
+class Shared(object):
+    def __init__(self):
+        self.semaphore = Semaphore(1)
+
+
+def read_write():
+    rd_ls = LightSwitch()
+    sh = Shared()
+
+    def read(read_ls, shared):
+        while True:   
+            print("before read")
+            read_ls.lock(shared.semaphore)
+            print("inside read")
+            sleep(randint(1,10)/10)
+            read_ls.unlock(shared.semaphore)
+            print("outside read")
+
+    def write(shared):
+        while True:   
+            print("before of write")
+            shared.semaphore.wait()
+            sleep(randint(1,10)/10)
+            print("inside write")
+            shared.semaphore.signal()
+            print("outside write")
+
+    threads= []
+
+    for _ in range(5):
+        t = Thread(read,rd_ls,sh)
+        threads.append(t)
+    for _ in range(1):
+        t = Thread(write,sh)
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+read_write()
