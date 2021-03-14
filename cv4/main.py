a7d0cb36595ea279329c6651004d0db2fface256
patosmolar <patosmolar@gmail.com>
2021-03-14 20:45:12 +0100

    feat: Add PowerPlant implementation
    Add implementation of PowerPlant according to assigment

--- cv4/main.py
+++ cv4/main.py
@@ -3,19 +3,19 @@ from time import sleep
 from random import randint
 
 
-class LightSwitch(object):
+class LightSwitch():
     def __init__(self):
         self.mutex = Mutex()
         self.counter = 0
 
     def lock(self, semaphore):
         self.mutex.lock()
-        cou = self.counter
-        self.counter = 1
+        count = self.counter
+        self.counter += 1
         if self.counter == 1:
             semaphore.wait()
         self.mutex.unlock()
-        return cou
+        return count
 
     def unlock(self, semaphore):
         self.mutex.lock()
@@ -25,7 +25,7 @@ class LightSwitch(object):
         self.mutex.unlock()
 
 
-class PowerPlant(object):
+class PowerPlant():
     def __init__(self):
         self.ls_monitor = LightSwitch()
         self.ls_sensor = LightSwitch()
@@ -39,11 +39,12 @@ class PowerPlant(object):
             self.monitor_acess.signal()
             read_time = randint(40, 50) / 1000
             print(
-                'monit "d": pocet_citajucich_monitorov=d, trvanie_citania=f\n'
+                'monit "%02d": pocet_citajucich_monitorov=%02d, '
+                'trvanie_citania=%03f\n'
                 % (monitor_id, n_monitors_reading, read_time)
             )
             sleep(read_time)
-            self.monitor_ls.unlock(self.no_sensor)
+            self.ls_monitor.unlock(self.sensor_acess)
 
     def sensor(self, sensor_id, actualisation_time):
         while True:
@@ -51,10 +52,31 @@ class PowerPlant(object):
             n_sensors_writing = self.ls_sensor.lock(self.monitor_acess)
             self.sensor_acess.wait()
             print(
-                'cidlo "d": pocet_zapisujucich_cidiel=d, trvanie_zapisu=f\n'
+                'cidlo "%02d": pocet_zapisujucich_cidiel=%02d, '
+                'trvanie_zapisu=%03f\n'
                 % (sensor_id, n_sensors_writing, actualisation_time)
             )
             sleep(actualisation_time)
 
             self.sensor_acess.signal()
             self.ls_sensor.unlock(self.monitor_acess)
+
+
+p = PowerPlant()
+threads = []
+for i in range(2):
+    sensor_pt_time = randint(10, 20) / 1000
+    t = Thread(p.sensor, i, sensor_pt_time)
+    threads.append(t)
+
+for i in range(1):
+    sensor_h_time = randint(20, 25) / 1000
+    t = Thread(p.sensor, i + 2, sensor_h_time)
+    threads.append(t)
+
+for i in range(8):
+    t = Thread(p.monitor, i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
