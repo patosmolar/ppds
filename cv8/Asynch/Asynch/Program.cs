f791033c830ba63aa44e18eab74caa89c7df96c8
patosmolar <patosmolar@gmail.com>
2021-04-19 22:18:21 +0200

    fix: Fix FryEggs function to really wait 6000ms

--- cv8/Asynch/Asynch/Program.cs
+++ cv8/Asynch/Asynch/Program.cs
@@ -75,10 +75,10 @@ namespace AsyncBreakfast
         private static Egg FryEggs(int howMany)
         {
             Console.WriteLine("Warming the egg pan...");
-            Task.Delay(3000);
+            Task.Delay(3000).Wait(); ;
             Console.WriteLine($"cracking {howMany} eggs");
             Console.WriteLine("cooking the eggs ...");
-            Task.Delay(3000);
+            Task.Delay(3000).Wait(); ;
             Console.WriteLine("Put eggs on plate");
 
             return new Egg();
