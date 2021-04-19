f63cb30098c7acd56cadaffe0cf56253de9fd5f7
patosmolar <patosmolar@gmail.com>
2021-04-19 22:32:00 +0200

    feat: Change FryBacon to be asynchronous

--- cv8/Asynch/Asynch/Program.cs
+++ cv8/Asynch/Asynch/Program.cs
@@ -56,17 +56,17 @@ namespace AsyncBreakfast
             return new Toast();
         }
 
-        private static Bacon FryBacon(int slices)
+        private static async Task<Bacon> FryBacon(int slices)
         {
             Console.WriteLine($"putting {slices} slices of bacon in the pan");
             Console.WriteLine("cooking first side of bacon...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             for (int slice = 0; slice < slices; slice++)
             {
                 Console.WriteLine("flipping a slice of bacon");
             }
             Console.WriteLine("cooking the second side of bacon...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Put bacon on plate");
 
             return new Bacon();
